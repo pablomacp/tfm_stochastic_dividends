779c42f9c0d0b9b7832812d00591322e6e9eec21
Pablo Macías <pablo_motril@hotmail.com>
2020-04-23 10:11:50 +0200

    Anaconda test

---
+++ python_lib/anaconda_test/hello.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 23 03:27:34 2020
+
+@author: pablo
+"""
+
+# Demo file for Spyder Tutorial
+# Hans Fangohr, University of Southampton, UK
+
+def hello():
+    """Print "Hello World" and return None."""
+    print("Hello World")
+
+# Main program starts here
+hello()
+
+
+import matplotlib.pyplot as plt
+
+plt.plot([2, 4], [5, 7],'o')
